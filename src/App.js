806882d656b396417a1b01ad1e631f99086120ce
aayushtibra <aayushtibra1997@gmail.com>
2019-09-16 17:36:37 +0530

    Add material ui
    Add appbar and other component from material ui

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import TypoGraphy from "@material-ui/core/Typography";
+import NavBar from "./navbar";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <AppBar color="primary" position="static">
+          <Toolbar>
+            <TypoGraphy color="inherit" variant="button">
+              My header
+            </TypoGraphy>
+            <TypoGraphy color="inherit" variant="button">
+              <NavBar />
+            </TypoGraphy>
+          </Toolbar>
+        </AppBar>
+      </div>
+    );
+  }
 }
 
 export default App;
